570a8e5e41bf4458c99a0986dccdc02b996fc998
Roy3443 <21b43.roy@sjec.ac.in>
2024-06-23 18:25:20 +0530

    create post to save new book

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,46 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT , mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app  = express();
+app.use(express.json());
 
+app.get('/', (request, response)=>{
+    console.log(request)
+    response.status(234).send('welcome to book store')
+});
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port : ${PORT}`);
+app.post('/books', async(request, response)=> {
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) 
+        {
+            return response.status(400).send({message:'Send all required fields : title, author, publishYear'});
+        }
+        const newBook = {
+            title : request.body.title,
+            author : request.body.author,
+            publishYear :request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+    }
+    catch{
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+mongoose.connect(mongoDBURL)
+.then(()=> {
+    console.log('App connected to database')
+    app.listen(PORT, () => {
+        console.log(`App is listening to port : ${PORT}`);
+    });
+})
+.catch((error)=> {
+    console.log(error);
 });
