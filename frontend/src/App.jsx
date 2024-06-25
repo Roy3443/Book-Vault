4c2a0978ced2d7f8bca45cf8018c0a884081ccff
Roy3443 <21b43.roy@sjec.ac.in>
2024-06-25 22:12:32 +0530

    SPA and Add react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import CreateBook from './pages/CreateBook';
+import DeleteBook from './pages/DeleteBook';
+import EditBook from './pages/EditBook';
+import ShowBook from './pages/ShowBook';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white border-spacing-5'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />}/>
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
 
