54ffc5a2459be2dbd56aa5d324d4bfa84634694e
Roy3443 <21b43.roy@sjec.ac.in>
2024-09-21 16:00:50 +0530

    modified routes

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -1,5 +1,5 @@
-import express from 'epress';
-import { Book } from '../models/bookModel';
+import express from 'express';
+import { Book } from '../models/bookModel.js';
 const router = express.Router();
 
 
