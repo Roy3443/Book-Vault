2abc4c4754c5ed9152a80ad105e2e9ff290f9fa2
Roy3443 <21b43.roy@sjec.ac.in>
2024-09-22 12:04:14 +0530

    config docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Exposes the frontend to the Docker network
+    port: 5173,      // Ensure this matches the port in docker-compose.yml
+  },
   plugins: [react()],
 })
