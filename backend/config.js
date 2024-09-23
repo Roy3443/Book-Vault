eae9fc0bfb2d77aee63bca3643e8fb12a60de1c5
Roy3443 <21b43.roy@sjec.ac.in>
2024-09-23 15:20:44 +0530

    add config file

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://royboy:<replace_your_password>@cluster0.vtzogqn.mongodb.net/<your_collection_name>?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
