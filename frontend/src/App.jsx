fc915d47626af5e6ec8cfcfa646ad63a4ff820a0
Roy3443 <21b43.roy@sjec.ac.in>
2024-10-15 07:08:02 +0530

    modify routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+// src/App.jsx
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
 import CreateBook from './pages/CreateBook';
-import DeleteBook from './pages/DeleteBook';
 import EditBook from './pages/EditBook';
 import ShowBook from './pages/ShowBook';
-import Home from './pages/Home';
+import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='/books/details/:id' element={<ShowBook />}/>
-      <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBook />} />
-    </Routes>
-  )
-}
+    <div>
+      <Navbar />
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/books/create" element={<CreateBook />} />
+          <Route path="/books/details/:id" element={<ShowBook />} />
+          <Route path="/books/edit/:id" element={<EditBook />} />
+          <Route path="/books/delete/:id" element={<DeleteBook />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App;
