06dc5d11af036e81fbd9657a2fe752901d6a36e9
Roy Lasrado <106914324+Roy3443@users.noreply.github.com>
2024-10-19 17:18:17 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -34,7 +34,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`https://book-vault-backend.vercel.app/books/${id}`, data)
       .then(() => {
         setLoading(false);
         navigate('/');
